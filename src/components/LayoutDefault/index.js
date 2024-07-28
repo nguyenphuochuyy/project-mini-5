346d0cbf2d2eb0957c96f1fcdd22e4c0c2e1af80
nguyenphuochuyy <phuochuy10023@gmail.com>
2024-07-28 22:14:44 +0700

    comple te signup

--- src/components/LayoutDefault/index.js
+++ src/components/LayoutDefault/index.js
@@ -1,27 +1,22 @@
 import { Layout } from "antd"
 import './layout.scss'
 import { Link, Outlet } from "react-router-dom"
+import ListTopic from "../ListTopic"
 const {Content , Footer} = Layout
 
 function LayoutDefault(){
     return (
         <>
-          <Layout>
+          <Layout className="layout">
               <header className="header">
                     <Link className="header__logo" to={"/"}>QUIZ</Link>
-                       <ul className="header__menu">
-                            <li className="header__menu-item">HTML</li>
-                            <li className="header__menu-item">HTML</li>
-                            <li className="header__menu-item">HTML</li>
-                            <li className="header__menu-item">HTML</li>
-                            <li className="header__menu-item">HTML</li>
-                       </ul>
+                        <ListTopic/>
                  <div className="header__user">
                     <Link  className="header__user-reg" to={"/sign-up"}>Sign Up</Link>
                     <div className="header__user-login">Login</div>
                  </div>
               </header>
-              <Content>
+              <Content className="content" >
                  <Outlet/>
               </Content>
               <Footer className="footer">Copyright @2024 by Huy</Footer>
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,8 +1,18 @@
+import "./reg.scss"
+import "../../assets/base.scss"
+import { Col, Form, Input, Row } from "antd";
 function Register(){
     return(
         <>
             <div className="sign-up">
-                asjkdhajsk
+                <form className="sign-up__form">
+                    <h2 className="sign-up__title">SignUp</h2>
+                    <input placeholder="Enter your email" className="input" name="email"></input>
+                    <input placeholder="Create a password" className="input" name="pass"></input>
+                    <input placeholder="confirm your password" className="input" name="confirm-pass"></input>
+                    <input type="submit" placeholder="Enter your email" className="input input__btn" value={"SignUp"}></input>
+                    
+                </form>
             </div>
         </>
     )
