993e86a3a262b0eb96acda3ce310e230acea45d6
nguyenphuochuyy <phuochuy10023@gmail.com>
2024-08-07 20:41:39 +0700

    complete answer pages

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getAnswerById, getQuestion, getQuestionById } from "../../helper/fetchApi";
-
+import { Radio, Space } from "antd";
+import "./result.scss"
 function Result(){
     const params = useParams();
     const [finallResult , setFinalResult] = useState([]);
-   
+    const [correctAns , setCorrectAns] = useState();
 
     useEffect(()=>{
         //call api để lấy câu trả lời của user by id
@@ -24,12 +25,65 @@ function Result(){
                         
         }
         fetchApi();
-     
+        
+        
     },[])    
+    console.log(finallResult);
+
+    
+    
     return(
         <>
             <div className="result">
+            <form  >
+            { finallResult.map((item,index) =>(
+                    <div className="result__item" key={item.id}>
+                         <div className="result__question"> Câu {item.id}:{item.question}  { item.answer === item.correctAnswer ? (<><span className="result__item--true">Đúng</span></>) : (<><span className="result__item--false">Sai</span></>)  }</div>
+                            {/* {item.answers.map((itemAns , indexAns) =>{
+                                let className = "";
+                                let checked = false;
+                                if(item.answer === index){
+                                    checked = true;
+                                    className = "result__item--selected"
+                                }
+                                    return(
+                                        <Radio.Group name={item.id}>
+                                        <Space direction="vertical">
+                                         <Radio className="result__body"  value={indexAns}>{item.answers[indexAns]}</Radio>
+                                         </Space>
+                                        </Radio.Group>
+                                    )
+                            })
+                                
+                                } */}
+                            <Radio.Group name={item.id} value={item.answer}>
+                                 <Space direction="vertical">
+                                {item.answers.map((itemAns , indexAns) => {
+                                 let className = "";
+                                    
+                                 if(item.answer === indexAns){
+                                    className = "result__item--selected";
+                                  
+                                 }
+                                 if(item.correctAnswer === indexAns){
+                                    className = "result__item--correct"
+                                 }
+                            return (
+                                <Radio className={`result__body ${className}`} key={indexAns} value={indexAns}>
+                                {itemAns}
+                                </Radio>
+                    );
+                })}
+                                 </Space>
+                            </Radio.Group>
+                         
+                </div>
                 
+            )) }
+            {/* <Button className="practice__btn"  htmlType="submit">Nộp bài</Button> */}
+            
+            </form>
+            
             </div>
         </>
     )
